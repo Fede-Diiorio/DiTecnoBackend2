027d163438dada27a860e8ecf70bd1754d024a9c
Federico Di Iorio <federico_metallica@hotmail.com>
2025-01-16 19:24:30 -0300

    Se modificarion tablas y se cargaron registros de puertas

--- src/dao/sql/models/crateDatabae.sql
+++ src/dao/sql/models/crateDatabae.sql
@@ -31,8 +31,49 @@ CREATE TABLE IF NOT EXISTS colors (
 CREATE TABLE IF NOT EXISTS windowss (
     id_window INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_opening INT NOT NULL,
-    id_type INT NOT NULL,
     id_style INT NOT NULL,
+    id_type INT NOT NULL,
+    id_color INT NOT NULL,
     casement_quantity INT NOT NULL DEFAULT 1,
-    is_window BOOLEAN NOT NULL
+    FOREIGN KEY (id_opening) REFERENCES opening (id_opening) ON DELETE CASCADE,
+    FOREIGN KEY (id_style) REFERENCES window_styles (id_style) ON DELETE CASCADE,
+    FOREIGN KEY (id_type) REFERENCES window_types (id_type) ON DELETE CASCADE,
+    FOREIGN KEY (id_color) REFERENCES colors (id_color) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS door_styles (
+    id_style INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_style VARCHAR (50) NOT NULL,
+    slug_style VARCHAR (50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS door_types (
+    id_type INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_type VARCHAR (70) NOT NULL,
+    slug_type VARCHAR (30) NOT NULL UNIQUE,
+    image_link VARCHAR (200)
+);
+
+CREATE TABLE IF NOT EXISTS designs (
+    id_design INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_design VARCHAR (40) NOT NULL,
+    slug_design VARCHAR (30) NOT NULL UNIQUE,
+    image_link VARCHAR (200)
+);
+
+CREATE TABLE IF NOT EXISTS doors (
+    id_door INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_opening INT NOT NULL,
+    id_style INT NOT NULL,
+    id_type INT NOT NULL,
+    id_color INT NOT NULL,
+    id_design INT NOT NULL,
+    casement_quantity INT NOT NULL DEFAULT 1,
+    casement_name VARCHAR (40),
+    FOREIGN KEY (id_opening) REFERENCES opening (id_opening) ON DELETE CASCADE,
+    FOREIGN KEY (id_style) REFERENCES door_styles (id_style) ON DELETE CASCADE,
+    FOREIGN KEY (id_type) REFERENCES door_types (id_type) ON DELETE CASCADE,
+    FOREIGN KEY (id_color) REFERENCES colors (id_color) ON DELETE CASCADE,
+    FOREIGN KEY (id_design) REFERENCES designs (id_design) ON DELETE CASCADE
+);
+
