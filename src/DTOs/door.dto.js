a7b8ce5ae43187f2c30aab23911994f047151151
Federico Di Iorio <federico_metallica@hotmail.com>
2025-01-18 07:08:10 -0300

    modificación de DTOs para mailing

--- src/DTOs/door.dto.js
+++ src/DTOs/door.dto.js
@@ -1,16 +1,16 @@
 export default class DoorDTO {
-    constructor(data, type) {
-        this.product = data.product;
-        this.productName = type.name;
-        this.opening = data.opening;
-        this.image = type.image;
-        this.design = data.design;
-        this.colors = data.colors;
-        this.width = `${data.width} cm`;
-        this.height = `${data.height} cm`;
-        this.casement = data.casement ? `${data.casement} cm` : 'No requerido';
-        this.casement2 = data.casement2 ? `${data.casement2} cm` : 'No requerido';
-        this.casement3 = data.casement3 ? `${data.casement3} cm` : 'No requerido';
-        this.quantity = data.quantity;
-    };
-};
+  constructor(data, type) {
+    this.product = data.product;
+    this.productName = type.name;
+    this.opening = data.opening;
+    this.image = type.image;
+    this.design = data.design;
+    this.colors = data.colors;
+    this.width = `${data.width} m`;
+    this.height = `${data.height} m`;
+    this.casement = data.casement ? `${data.casement} m` : "No requerido";
+    this.casement2 = data.casement2 ? `${data.casement2} m` : "No requerido";
+    this.casement3 = data.casement3 ? `${data.casement3} m` : "No requerido";
+    this.quantity = data.quantity;
+  }
+}
--- src/DTOs/window.dto.js
+++ src/DTOs/window.dto.js
@@ -1,15 +1,15 @@
 export default class WindowDTO {
-    constructor(data, type) {
-        this.product = data.product;
-        this.opening = data.opening;
-        this.image = type.image;
-        this.style = data.style;
-        this.colors = data.colors;
-        this.width = `${data.width} cm`;
-        this.height = `${data.height} cm`;
-        this.casement = data.casement ? `${data.casement} cm` : 'No requerido';
-        this.casement2 = data.casement2 ? `${data.casement2} cm` : 'No requerido';
-        this.glassType = data.glassType;
-        this.quantity = data.quantity;
-    };
-};
+  constructor(data, type) {
+    this.product = data.product;
+    this.opening = data.opening;
+    this.image = type.image;
+    this.style = data.style;
+    this.colors = data.colors;
+    this.width = `${data.width} m`;
+    this.height = `${data.height} m`;
+    this.casement = data.casement ? `${data.casement} m` : "No requerido";
+    this.casement2 = data.casement2 ? `${data.casement2} m` : "No requerido";
+    this.glassType = data.glassType;
+    this.quantity = data.quantity;
+  }
+}
