e0d0d654e025fdaf4b8146ae8e7b959a841e6279
Federico Di Iorio <federico_metallica@hotmail.com>
2025-01-19 09:10:24 -0300

    Cambios generales en la BBDD

--- src/dao/sql/models/crateDatabae.sql
+++ src/dao/sql/models/crateDatabae.sql
@@ -7,58 +7,26 @@ CREATE TABLE IF NOT EXISTS opening (
     slug_opening VARCHAR (30) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS window_styles (
-	id_style INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name_style VARCHAR (50) NOT NULL,
-    slug_style VARCHAR (50) NOT NULL UNIQUE,
-    image_link VARCHAR (200)
+CREATE TABLE IF NOT EXISTS styles (
+    id_style INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_style VARCHAR(50) NOT NULL,
+    slug_style VARCHAR(50) NOT NULL UNIQUE,
+    type_style ENUM('window', 'door') NOT NULL,
+    image_link VARCHAR(200)
 );
 
-CREATE TABLE IF NOT EXISTS window_types (
-	id_type INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name_type VARCHAR (70) NOT NULL,
-    slug_type VARCHAR (30) NOT NULL UNIQUE,
-    image_link VARCHAR (200)
+CREATE TABLE IF NOT EXISTS typess (
+    id_type INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_type VARCHAR(70) NOT NULL,
+    slug_type VARCHAR(30) NOT NULL UNIQUE,
+    type_type ENUM('window', 'door') NOT NULL, 
+    image_link VARCHAR(200)
 );
 
 CREATE TABLE IF NOT EXISTS colors (
     id_color INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name_color VARCHAR (50) NOT NULL,
     slug_color VARCHAR (50) NOT NULL UNIQUE,
-    image_link VARCHAR (200)
-);
-
-CREATE TABLE IF NOT EXISTS windowss (
-    id_window INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_opening INT NOT NULL,
-    id_style INT NOT NULL,
-    id_type INT NOT NULL,
-    id_color INT NOT NULL,
-    casement_quantity INT NOT NULL DEFAULT 1,
-    min_height DECIMAL(3,2) NOT NULL,
-    max_height DECIMAL(3,2) NOT NULL,
-    min_width DECIMAL(3,2) NOT NULL,
-    max_width DECIMAL(3,2) NOT NULL,
-    min_width_casement DECIMAL(3,2),
-    max_width_casement DECIMAL(3,2),
-    min_height_casement DECIMAL(3,2),
-    max_height_casement DECIMAL(3,2),
-    FOREIGN KEY (id_opening) REFERENCES opening (id_opening) ON DELETE CASCADE,
-    FOREIGN KEY (id_style) REFERENCES window_styles (id_style) ON DELETE CASCADE,
-    FOREIGN KEY (id_type) REFERENCES window_types (id_type) ON DELETE CASCADE,
-    FOREIGN KEY (id_color) REFERENCES colors (id_color) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS door_styles (
-    id_style INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name_style VARCHAR (50) NOT NULL,
-    slug_style VARCHAR (50) NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS door_types (
-    id_type INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name_type VARCHAR (70) NOT NULL,
-    slug_type VARCHAR (30) NOT NULL UNIQUE,
     image_link VARCHAR (200)
 );
 
@@ -69,19 +37,27 @@ CREATE TABLE IF NOT EXISTS designs (
     image_link VARCHAR (200)
 );
 
-CREATE TABLE IF NOT EXISTS doors (
-    id_door INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS products (
+    id_product INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_opening INT NOT NULL,
     id_style INT NOT NULL,
     id_type INT NOT NULL,
     id_color INT NOT NULL,
-    id_design INT NOT NULL,
+    id_design INT,
     casement_quantity INT NOT NULL DEFAULT 1,
-    casement_name VARCHAR (40),
+    casement_name VARCHAR(40),
+    min_height DECIMAL(3,2) NOT NULL,
+    max_height DECIMAL(3,2) NOT NULL,
+    min_width DECIMAL(3,2) NOT NULL,
+    max_width DECIMAL(3,2) NOT NULL,
+    min_width_casement DECIMAL(3,2),
+    max_width_casement DECIMAL(3,2),
+    min_height_casement DECIMAL(3,2),
+    max_height_casement DECIMAL(3,2),
+    product_type ENUM('window', 'door') NOT NULL,
     FOREIGN KEY (id_opening) REFERENCES opening (id_opening) ON DELETE CASCADE,
-    FOREIGN KEY (id_style) REFERENCES door_styles (id_style) ON DELETE CASCADE,
-    FOREIGN KEY (id_type) REFERENCES door_types (id_type) ON DELETE CASCADE,
+    FOREIGN KEY (id_style) REFERENCES styles (id_style) ON DELETE CASCADE,
+    FOREIGN KEY (id_type) REFERENCES typess (id_type) ON DELETE CASCADE,
     FOREIGN KEY (id_color) REFERENCES colors (id_color) ON DELETE CASCADE,
     FOREIGN KEY (id_design) REFERENCES designs (id_design) ON DELETE CASCADE
 );
-
--- src/dao/sql/window.dao.js
+++ src/dao/sql/window.dao.js
@@ -16,18 +16,18 @@ export default class WindowDao {
   async getStyles(opening) {
     const [styles] = await db.query(
       ` SELECT
-            s.id_style,
             s.name_style,
             s.slug_style,
             s.image_link
-        FROM windowss AS w
-        INNER JOIN window_styles AS s ON (s.id_style = w.id_style)
-        INNER JOIN opening AS o ON (o.id_opening = w.id_opening)
-        WHERE o.slug_opening = ?
+        FROM products AS p
+        INNER JOIN styles AS s ON (s.id_style = p.id_style)
+        INNER JOIN opening AS o ON (o.id_opening = p.id_opening)
+        WHERE o.slug_opening = 'interior'
+        AND s.type_style = "window"
         GROUP BY
             s.name_style,
             s.slug_style,
-            s.image_link`,
+            s.image_link;`,
       [opening]
     );
     return styles;
@@ -35,22 +35,24 @@ export default class WindowDao {
 
   async getTypes(opening, style) {
     const [types] = await db.query(
-      ` SELECT
-            t.id_type,
-            t.name_type,
-            t.slug_type,
-            t.image_link
-        FROM windowss AS w
-        INNER JOIN opening AS o ON (o.id_opening = w.id_opening)
-        INNER JOIN window_styles AS s ON (s.id_style = w.id_style)
-        INNER JOIN window_types AS t ON (t.id_type = w.id_type)
-        WHERE o.slug_opening = ? 
-        AND s.slug_style = ?
-        GROUP BY
-            t.id_type,
-            t.name_type,
-            t.slug_type,
-            t.image_link;`,
+      `
+      SELECT
+          t.name_type,
+          t.slug_type,
+          t.image_link
+      FROM products AS p
+      INNER JOIN opening AS o ON (o.id_opening = p.id_opening)
+      INNER JOIN styles AS s ON (s.id_style = p.id_style)
+      INNER JOIN typess AS t ON (t.id_type = p.id_type)
+      WHERE o.slug_opening = ? 
+      AND s.slug_style = ?
+      AND s.type_style = "window"
+      AND t.type_type = "window"
+      GROUP BY
+          t.name_type,
+          t.slug_type,
+          t.image_link;
+      `,
       [opening, style]
     );
     return types;
@@ -62,35 +64,36 @@ export default class WindowDao {
           t.name_type,
           t.slug_type,
           t.image_link,
-          w.casement_quantity,
-          w.min_height,
-          w.max_height,
-          w.min_width,
-          w.max_width,
-          w.min_height_casement,
-          w.max_height_casement,
-          w.min_width_casement,
-          w.max_width_casement
-        FROM windowss AS w
-        INNER JOIN opening AS o ON (o.id_opening = w.id_opening)
-        INNER JOIN window_styles AS s ON (s.id_style = w.id_style)
-        INNER JOIN window_types AS t ON (t.id_type = w.id_type)
+          p.casement_quantity,
+          p.min_height,
+          p.max_height,
+          p.min_width,
+          p.max_width,
+          p.min_height_casement,
+          p.max_height_casement,
+          p.min_width_casement,
+          p.max_width_casement
+        FROM products AS p
+        INNER JOIN opening AS o ON (o.id_opening = p.id_opening)
+        INNER JOIN styles AS s ON (s.id_style = p.id_style)
+        INNER JOIN typess AS t ON (t.id_type = p.id_type)
         WHERE o.slug_opening = ?
         AND s.slug_style = ?
         AND t.slug_type = ?
+        AND p.product_type = "window"
         GROUP BY
           t.name_type,
           t.slug_type,
           t.image_link,
-          w.casement_quantity,
-          w.min_height,
-          w.max_height,
-          w.min_width,
-          w.max_width,
-          w.min_height_casement,
-          w.max_height_casement,
-          w.min_width_casement,
-          w.max_width_casement;`,
+          p.casement_quantity,
+          p.min_height,
+          p.max_height,
+          p.min_width,
+          p.max_width,
+          p.min_height_casement,
+          p.max_height_casement,
+          p.min_width_casement,
+          p.max_width_casement;`,
       [opening, style, type]
     );
     return typeSpecification[0];
